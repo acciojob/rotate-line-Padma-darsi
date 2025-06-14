926aec7696940980db952ebf2588795bf45e3efb
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-06-14 19:42:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const line=document.getElementById("line");
+let angle=0;
+setInterval(()=>
+	{
+		angle += 1;// increase angle
+		line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`; 
+	},20)
